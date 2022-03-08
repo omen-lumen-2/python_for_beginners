60cab991444eca53475e8e615322a7929f590a0a
user <nikolay.komarov@rt.ru>
2022-03-08 15:29:51 +0300

    add test

---
+++ tests/test_add_contact.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest
+
+from model.Contact import Contact
+
+
+class TestAddContact(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+    def test_add_new_groups(self):
+        wd = self.wd
+        self.open_authorize_page(wd)
+        self.login(wd, login="admin", password="secret")
+        self.create_new_contact(wd, Contact(
+            firstname="Test_first_name",
+            middlename="Test_middle_name",
+            email="test@test.test"
+        ))
+        self.go_to_home_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def go_to_home_page(self, wd):
+        wd.find_element_by_link_text("home").click()
+
+    def create_new_contact(self, wd, contact):
+        # move to create new contact
+        wd.find_element_by_link_text("add new").click()
+        # input firstname
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        # input middlename
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
+        # input email
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact.email)
+        # submit
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def login(self, driver, login, password):
+        # input login
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys(login)
+        driver.find_element_by_id("LoginForm").click()
+        # input password
+        driver.find_element_by_name("pass").click()
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys(password)
+        # submit
+        driver.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_authorize_page(self, driver):
+        driver.get("http://localhost/addressbook/group.php?delete=Delete+group%28s%29&selected%5B%5D=1")
+
+    def is_element_present(self, how, what):
+        try:
+            self.wd.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.wd.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
