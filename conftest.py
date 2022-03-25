cc95c1631e8fdfcad3e66e7702e2415eddea260c
user <nikolay.komarov@rt.ru>
2022-03-25 19:11:36 +0300

    implement 17 task

--- conftest.py
+++ conftest.py
@@ -1,21 +1,27 @@
+import json
+import os.path
+
 import pytest
 
 from fixture.application import Application
 
 application = None
+target = None
 
 
 @pytest.fixture()
 def app(request):
     """ Fixture for work with class Application in each test function"""
     global application
+    global target
     browser = request.config.getoption('--browser')
-    base_url = request.config.getoption("--base_url")
-    login = request.config.getoption("--login")
-    password = request.config.getoption("--password")
+    if target is None:
+        file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption('--config_file'))
+        with open(file) as config_file:
+            target = json.load(config_file)
     if application is None or application.is_not_valid():
-        application = Application(browser=browser, base_url=base_url)
-    application.session.ensure_login(login=login, password=password)
+        application = Application(browser=browser, base_url=target['base_url'])
+    application.session.ensure_login(login=target['login'], password=target['password'])
     return application
 
 
@@ -29,6 +35,4 @@ def stop(request):
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action='store', default='firefox', help='type of browser')
-    parser.addoption("--base_url", action='store', default='http://localhost/addressbook/', help='base_url')
-    parser.addoption("--login", action='store', default='admin', help='login admin')
-    parser.addoption("--password", action='store', default='secret', help='password')
+    parser.addoption("--config_file", action='store', default='config.json', help='file with config')
